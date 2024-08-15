3a1cbe38f48a148da5188bd5c8ac469805ad5a85
Alvarojohansen <johansenalvaro@gmail.com>
2024-08-14 23:17:02 -0300

    primer commit

---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "../navBar/NavBar";
+import Carousel from "../carousel/Carousel";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <div >
+        {/* <Carousel /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
