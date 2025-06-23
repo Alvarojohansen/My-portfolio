54336dcffde31860ce4f44b910f9d28d12e48ea7
Alvaro Johansen <109972764+Alvarojohansen@users.noreply.github.com>
2025-06-22 22:26:38 -0300

    add

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/My-portfolio/",
+  base: "https://Alvarojohansen.github.io/My-portfolio",
   plugins: [react()],
 });
